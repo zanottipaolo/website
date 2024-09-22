083a01f74cff9cddb1662d89d13a680d628529ce
Paolo Zanotti <60442209+zanottipaolo@users.noreply.github.com>
2024-09-23 01:26:41 +0200

    🐛 Fix filter category not working when change page

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,21 +3,18 @@ import { SITE } from "@consts";
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const blog = (await getCollection("blog")).filter((post) => !post.data.draft);
-
-  const projects = (await getCollection("projects")).filter(
-    (project) => !project.data.draft,
-  );
-
-  const items = [...blog, ...projects].sort(
-    (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
-  );
+  const posts = (await getCollection("blog"))
+    .filter((post) => !post.data.draft)
+    .sort(
+      (a, b) =>
+        new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
+    );
 
   return rss({
     title: SITE.TITLE,
     description: SITE.DESCRIPTION,
     site: context.site,
-    items: items.map((item) => ({
+    items: posts.map((item) => ({
       title: item.data.title,
       description: item.data.description,
       pubDate: item.data.date,
